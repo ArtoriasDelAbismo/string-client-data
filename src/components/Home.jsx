32a56260fefd1a1c267c827cdbc011bcb7d7570d
artorias <jerojournade1@gmail.com>
2025-05-26 10:33:20 -0300

    change color version

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
     <Navbar />
-        <h1>Tierra Database</h1>
+        <h1>TB Database</h1>
         <div style={{display:'flex', justifyContent:'center', gap:'10px'}}>
             <div>
                 <Link style={{width:'100px', height:'50px', border:'1px solid', borderRadius:'4px', display:'flex', alignItems:'center', justifyContent:'center', cursor:'pointer'}} to={'/strings'}>Strings</Link>
