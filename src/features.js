df9421a546002d09f62210b37d40b21a04626147
artorias <jerojournade1@gmail.com>
2025-07-03 13:49:50 -0300

    features

--- src/features.js
+++ src/features.js
@@ -1,2 +1,2 @@
 export const feature = ['Check button completes the entry and notify the client', 'Entries counter']
-export const bugFix = ['Persistent fullname input']
+export const bugFix = ['Fix input search']
