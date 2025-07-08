3ac287bdf4dae69f7133dfee2e985c43cb663d70
artorias <jerojournade1@gmail.com>
2025-07-08 08:51:26 -0300

    Home code refactoring

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,21 +2,22 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import "./Home.css";
+
+const links = [
+  { to: "/strings", text: "Strings", image: "/stringing-black-white.jpg" },
+  { to: "/workshop", text: "Workshop", image: "/broken_racquet_20111.png" },
+];
 
 export default function Home() {
-
   const [backgroundImage, setBackgroundImage] = useState(null);
   const [imageVisible, setImageVisible] = useState(false);
 
-    useEffect(() => {
-    const images = [
-      "/stringing-black-white.jpg",
-      "/broken_racquet_20111.png"
-    ];
-
-    images.forEach((src) => {
+  useEffect(() => {
+    // Preload images
+    links.forEach((link) => {
       const img = new Image();
-      img.src = src;
+      img.src = link.image;
     });
   }, []);
 
@@ -25,86 +26,33 @@ export default function Home() {
       <Navbar />
 
       <div
+        className="home-background"
         style={{
-          position: "fixed",
-          top: 0,
-          left: 0,
-          zIndex: 0,
           backgroundImage: backgroundImage ? `url(${backgroundImage})` : "none",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
           opacity: imageVisible ? 1 : 0,
-          transition: "opacity 0.5s ease-in-out",
-          height: "100vh",
-          width: "100vw",
-          pointerEvents: "none",
         }}
       />
 
-      <div
-        style={{
-          position: "relative",
-          zIndex: 1,
-          height: "100%",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-        }}
-      >
+      <div className="home-content">
         <h1>TB Database</h1>
-        <div style={{ display: "flex", justifyContent: "center", gap: "100px" }}>
-          <div>
-            <Link
-              onMouseEnter={() => {
-                setBackgroundImage("/stringing-black-white.jpg");
-                setImageVisible(true);
-              }}
-              onMouseLeave={() => {
-                setImageVisible(false);
-              }}
-              style={{
-                width: "100px",
-                height: "50px",
-                border: "1px solid",
-                borderRadius: "4px",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                cursor: "pointer",
-                textDecoration: "none",
-              }}
-              to={"/strings"}
-            >
-              Strings
-            </Link>
-          </div>
-          <div>
-            <Link
-              onMouseEnter={() => {
-                setBackgroundImage("/broken_racquet_20111.png");
-                setImageVisible(true);
-              }}
-              onMouseLeave={() => {
-                setImageVisible(false);
-              }}
-              style={{
-                width: "100px",
-                height: "50px",
-                border: "1px solid",
-                borderRadius: "4px",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                cursor: "pointer",
-                textDecoration: "none",
-              }}
-              to={"/workshop"}
-            >
-              Workshop
-            </Link>
-          </div>
+        <div className="home-links">
+          {links.map((link) => (
+            <div key={link.to}>
+              <Link
+                className="home-link"
+                to={link.to}
+                onMouseEnter={() => {
+                  setBackgroundImage(link.image);
+                  setImageVisible(true);
+                }}
+                onMouseLeave={() => {
+                  setImageVisible(false);
+                }}
+              >
+                {link.text}
+              </Link>
+            </div>
+          ))}
         </div>
       </div>
 
