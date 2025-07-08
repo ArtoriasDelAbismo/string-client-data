678efe69e4982e328117e828446e9c8eded2c6ec
artorias <jerojournade1@gmail.com>
2025-07-08 09:54:09 -0300

    Paid/Unpaid, code refactoring

--- src/features.js
+++ src/features.js
@@ -1,2 +1,2 @@
-export const feature = ['Check button completes the entry and notify the client', 'Entries counter']
-export const bugFix = ['Improve pagination', 'Fix total entries counter']
+export const feature = ['Paid/Unpaid button', 'Entries counter']
+export const bugFix = ['Code refactoring']
