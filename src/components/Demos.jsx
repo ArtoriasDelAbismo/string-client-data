e17ce129855ef5328b4001b49eaf65a3bb078757
artorias <jerojournade1@gmail.com>
2025-08-06 09:43:14 -0300

    mail demo

--- src/components/Demos.jsx
+++ src/components/Demos.jsx
@@ -289,11 +289,20 @@ export default function Demos() {
                                   handleToggleCheck(entry.id, entry.completed);
                                 }}
                               >
-                                <a
-                                  href={`mailto:${entry.mail}?subject=Demo&body=Hola ${entry.fullname}, tu raqueta demo ${entry.demo} está lista para ser retirada. Te esperamos!`}
-                                >
-                                  <i className="fa-solid fa-check"></i>
-                                </a>
+<a
+  href={`mailto:${entry.mail}?subject=Préstamo%20demo&body=Confirmación%20de%20préstamo%20de%20demo:%0A%0A` +
+    `Modelo:%20${entry.demo}%0A` +
+    `Fecha%20de%20retiro:%20${entry.date}%0A` +
+    `Monto%20de%20seña%20abonado:%20$10000%0A%0A` +
+    `Condiciones%20del%20préstamo:%0A` +
+    `- El%20plazo%20de%20préstamo%20es%20de%203%20días%20corridos%20a%20partir%20de%20la%20fecha%20de%20retiro.%0A` +
+    `- En%20caso%20de%20excederse%20en%20la%20devolución,%20se%20aplicará%20un%20cargo%20adicional%20de%20$5.000%20por%20cada%20día%20de%20demora.%0A` +
+    `- En%20caso%20de%20decidir%20la%20compra%20del%20producto,%20el%20monto%20abonado%20como%20seña%20será%20descontado%20del%20precio%20final%20de%20la%20raqueta.%0A%0A` +
+    `Ante%20cualquier%20consulta%20o%20para%20coordinar%20la%20devolución%20o%20adquisición%20del%20producto,%20quedamos%20a%20disposición.%0A%0ASaludos%0ATierra%20Batida%0A3425682136%0AAvellaneda%203199%20-%20Santa%20Fe`}
+>
+  <i className="fa-solid fa-check"></i>
+</a>
+
                               </button>
                             ) : (
                               <button
