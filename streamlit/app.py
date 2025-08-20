5ad909842c5e8583ef5d43744cc50689f30182f2
artorias <jerojournade1@gmail.com>
2025-08-20 07:32:13 -0300

    add scroll table

--- streamlit/app.py
+++ streamlit/app.py
@@ -2,20 +2,36 @@
 import pandas as pd
 import streamlit as st
 
+st.set_page_config(
+    page_title="CSV tool",
+    page_icon="favicon.png"
+)
+
+st.sidebar.title("Navigation")
+st.sidebar.title("Go to")
+
+st.sidebar.markdown("[Home](https://tb-workshop.netlify.app/)")
+st.sidebar.markdown("[Strings](https://tb-workshop.netlify.app/Strings)")
+st.sidebar.markdown("[Workshop](https://tb-workshop.netlify.app/Workshop)")
+st.sidebar.markdown("[Demos](https://tb-workshop.netlify.app/Demos)")
+
 
 st.title("CSV file managing tool")
 uploaded_file = st.file_uploader("Choose file to upload", type="csv")
 
 
 if uploaded_file is not None:
-    st.subheader("File details")
     df = pd.read_csv(uploaded_file, index_col=0)
-    st.write(df.head())
-    st.write(df.shape)
+    if st.button("Show details"):
+        st.subheader("File details")
+
+        st.write(f"Total rows: {len(df)}")
+        st.write(df.shape)
+        st.dataframe(df, use_container_width=True, height=600)
+    else: ""
 
 
-    columns = df["Nombre"].tolist()
-    st.write(columns)
+
     
 
 
