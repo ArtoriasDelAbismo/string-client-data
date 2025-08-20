25359f9352f731fe78aaa01ab3d272764952556d
artorias <jerojournade1@gmail.com>
2025-08-20 08:29:10 -0300

    add filtered list

--- streamlit/app.py
+++ streamlit/app.py
@@ -18,16 +18,25 @@ st.sidebar.markdown("[Demos](https://tb-workshop.netlify.app/Demos)")
 
 st.title("CSV file managing tool")
 uploaded_file = st.file_uploader("Choose file to upload", type="csv")
-
+if uploaded_file is None:
+    st.subheader("Export file from Smart Manager choosing 'SKU', 'Nombre', 'Inventario', 'Precio normal'")
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file, index_col=0)
+
+    
+
     if st.button("Show details"):
         st.subheader("File details")
+        filtered_df = df[df["Inventario"] > 0]
 
-        st.write(f"Total rows: {len(df)}")
-        st.write(df.shape)
-        st.dataframe(df, use_container_width=True, height=600)
+        st.write(f"Total Products: {len(df)}")
+        st.write(f"Existent products {len(filtered_df)}")
+
+
+
+        st.dataframe(filtered_df, use_container_width=True, height=600)
+        st.write(filtered_df.shape)
     else: ""
 
 
